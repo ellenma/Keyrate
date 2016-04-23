90c94e06eac35aba858a9e9fa59b5fd43ad33014
Zhiyuan Tang <zhiyuan.tang1@gmail.com>
2016-04-23 15:32:25 -0700

    fixed a bug

--- QECalculator.m
+++ QECalculator.m
@@ -8,10 +8,10 @@ end
 
 
 Qr = (counts(1, 2) + counts(1, 3) + counts(2, 2) + counts(2, 3)) /...
-    synRecordNum / (counts(1, 1) + counts(2, 1));
+    (0.5 * synRecordNum * (counts(1, 1) + counts(2, 1)));
 
 Qd = (counts(3, 4) + counts(3, 5) + counts(4, 4) + counts(4, 5)) /...
-    synRecordNum / (counts(3, 1) + counts(4, 1));
+    (0.5 * synRecordNum * (counts(3, 1) + counts(4, 1)));
 
 Er = (counts(1, 3) + counts(2, 2)) /...
     (counts(1, 2) + counts(1, 3) + counts(2, 2) + counts(2, 3));
