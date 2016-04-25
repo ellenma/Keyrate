c10964a9a0f8d019df433fc98e3d61bad7ab6c35
ellenma <ellenmachaoxuan@gmail.com>
2016-04-25 02:38:30 -0300

    Update NoDecoyKeyRate.m

--- NoDecoyKeyRate.m
+++ NoDecoyKeyRate.m
@@ -4,5 +4,8 @@ E = 0.0542
 mu = 0.024
 Omega = 1 - pMulti(mu) / Q
 f = 1.16
+Counts = dlmread('NoDecoyQKDCounts.txt','\t','A2..E5');
+p=(sum(Counts(2:3,1))/sum(Counts(2:5,1))) * (sum(Counts(2:3,2:3))/sum(Counts(1:2,2:5)))+...
+(sum(Counts(4:5,1))/sum(Counts(2:5,1))) * (sum(Counts(4:5,2:3))/sum(Counts(4:5,2:5)));%Prob(Alice and Bob use compatible bases)
 
 R = Q * (-h(E) * f + Omega * (1 - h(E / Omega))) * 1e7
